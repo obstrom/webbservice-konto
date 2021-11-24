5a0cf65f4bdcc22c3d6c3de3d4a2aeb76735d46b
Oscar Bergström <oscar@obstrom.com>
2021-11-24 12:37:39 +0100

    Initial commit

---
+++ src/main/java/com/example/webbservicekonto/WebbserviceKontoApplication.java
@@ -0,0 +1,13 @@
+package com.example.webbservicekonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbserviceKontoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebbserviceKontoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webbservicekonto/WebbserviceKontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webbservicekonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbserviceKontoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
