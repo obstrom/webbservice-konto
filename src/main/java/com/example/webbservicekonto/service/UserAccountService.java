08da55bf97754dda23f25a1d8f5725f68d4867cb
Oscar Bergström <oscar@obstrom.com>
2021-11-29 11:15:30 +0100

    Updated service layer with New and Update methods

--- src/main/java/com/example/webbservicekonto/service/UserAccountService.java
+++ src/main/java/com/example/webbservicekonto/service/UserAccountService.java
@@ -2,9 +2,11 @@ package com.example.webbservicekonto.service;
 
 import com.example.webbservicekonto.model.UserAccount;
 import com.example.webbservicekonto.repository.UserAccountRepository;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -27,26 +29,62 @@ public class UserAccountService {
                 .orElseThrow(() -> new IllegalStateException("no user account with ID '" + id + "' found!"));
     }
 
-    public void registerNewUserAccount( /* Need to know what fields User model has to take in */ ) {
-        // TODO - Need to add proper validation, can only be done when User model is finished
+    // TODO
+    //  - Add method "getUsersByName()" ?
+    //  - Add method "getUsersByEmail()" ?
+
+    public void registerNewUserAccount(UserAccount userAccount) {
+        if (isStringEmpty(userAccount.getName()))
+            throw new IllegalStateException("new user account requires a name");
+
+        if (isStringEmpty(userAccount.getEmail()))
+            throw new IllegalStateException("new user account requires an email");
+
+        if (isStringEmpty(userAccount.getPassword()))
+            throw new IllegalStateException("new user account requires a password");
+
+        userAccountRepository.save(userAccount);
     }
 
-    // TODO
-    //  - Add method "findUsersByName()" ?
-    //  - Add method "findUsersByEmail()" ?
+    @Transactional
+    public void updateUserAccount(@NonNull Long id, String newName, String newEmail, String newPassword) {
+        boolean anyFieldsUpdated = false;
+        UserAccount account = userAccountRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("no user account with ID '" + id + "' found!"));
 
-    public void updateUserAccount( /* Need to know what fields User model has to take in */ ) {
-        // TODO - Need to add proper validation, can only be done when User model is finished
+        if (!isStringEmpty(newName) && isStringsNotEqual(account.getName(), newName)) {
+            account.setName(newName);
+            anyFieldsUpdated = true;
+        }
+
+        if (!isStringEmpty(newEmail) && isStringsNotEqual(account.getEmail(), newEmail)) {
+            account.setEmail(newEmail);
+            anyFieldsUpdated = true;
+        }
+
+        if (!isStringEmpty(newPassword) && isStringsNotEqual(account.getPassword(), newPassword)) {
+            // TODO - Add Security?
+            //  - Hash and salt?
+            account.setPassword(newPassword);
+            anyFieldsUpdated = true;
+        }
+
+        if (anyFieldsUpdated) userAccountRepository.save(account);
     }
-
-    // TODO
-    //  - Add method "updatePassword()" ?
 
     public void deleteUserAccount(Long id) {
         if (!userAccountRepository.existsById(id))
             throw new IllegalStateException("no account with ID '" + id + "' found!");
 
         userAccountRepository.deleteById(id);
+    }
+
+    private boolean isStringEmpty(String string) {
+        return string == null || string.equals("");
+    }
+
+    private boolean isStringsNotEqual(String string1, String string2) {
+        return string1.strip().equals(string2.strip());
     }
 
 }
