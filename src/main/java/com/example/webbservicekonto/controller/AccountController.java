4be553a0fbd9650157f84b8c87d56d32df04ab82
Admir Hamzic <admirhamzic92@live.com>
2021-12-02 13:32:11 +0100

    Added + new dependencies + Crossorigin in AccountController class (change localhost) + password encoder in UserAccountService

--- src/main/java/com/example/webbservicekonto/controller/AccountController.java
+++ src/main/java/com/example/webbservicekonto/controller/AccountController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:xxxx", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:8080", maxAge = 3600)
 @RequestMapping("/api/v1")
 
 public class AccountController {
