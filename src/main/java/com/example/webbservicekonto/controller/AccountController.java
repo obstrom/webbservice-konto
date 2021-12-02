23b6faeb8273b5c1dd1d21e2e655c906ec7b9cfc
Oscar Bergström <oscar@obstrom.com>
2021-12-02 13:13:25 +0100

    Added support to Controller for returning both JSON and XML

--- src/main/java/com/example/webbservicekonto/controller/AccountController.java
+++ src/main/java/com/example/webbservicekonto/controller/AccountController.java
@@ -5,6 +5,7 @@ import com.example.webbservicekonto.service.UserAccountService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,28 +19,28 @@ public class AccountController {
     @Autowired
     private UserAccountService UserAccountService;
 
-    @GetMapping("/users")
+    @GetMapping(path = "/users", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public ResponseEntity<UserAccountResponse> getAllUsers() {
         List<UserAccount> allUserAccounts = UserAccountService.findAllUserAccounts();
 
         return ResponseEntity.ok().body(new UserAccountResponse(HttpStatus.OK, "Success retrieving all user accounts", allUserAccounts));
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping(path = "/users/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public ResponseEntity<UserAccountResponse> getUsersById(@PathVariable(value = "id") Long userId) {
         UserAccount userAccount = UserAccountService.findUserAccount(userId);
 
         return ResponseEntity.ok().body(new UserAccountResponse(HttpStatus.OK, "User account with ID " + userId + " found", userAccount));
     }
 
-    @PostMapping("/users")
+    @PostMapping(path = "/users", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
     public ResponseEntity<UserAccountResponse> createUser(@RequestBody UserAccount user) {
         UserAccount userAccount = UserAccountService.registerNewUserAccount(user);
 
         return ResponseEntity.ok().body(new UserAccountResponse(HttpStatus.OK, "New account created successfully", userAccount));
     }
 
-    @PutMapping("/users/{id}")
+    @PutMapping(path = "/users/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
     public ResponseEntity<UserAccountResponse> updateUser(@PathVariable(value = "id") Long userId, @RequestBody UserAccount userDetails) {
         UserAccount userAccount = UserAccountService.updateUserAccount(userId, userDetails.getName(), userDetails.getEmail(), userDetails.getPassword());
 
